4636cacf1c7ed43b173352668006bf16d8d6c10f
mala23 <mail@mala23.me>
2019-04-02 13:47:36 +0200

    display map

---
+++ mapbox.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3NjaG1pZC16aGRrIiwiYSI6ImNqb2oxNXMyZTAwY3kzcXBoeGZ3MnlvOXoifQ.dcxa3bz9-6M-7F7aL_Yrtw'
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/cschmid-zhdk/cjtzn1c3w4rij1fozb3o2czfp',
+    center: [-87.661557, 41.893748],
+    zoom: 10.7
+
+})
